92d94c0b40b26aaaf826563556355e0532044dad
Jim <jimhlad@gmail.com>
2017-08-05 10:39:17 -0400

    Update dashboard.blade.php

--- src/Views/dashboard.blade.php
+++ src/Views/dashboard.blade.php
@@ -5,7 +5,7 @@
 		@include('leapfrog::sidebar')
 		<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
 			<h1 class="page-header">Changelog</h1>
-			<h2>0.0.1</h2>
+			<h2>1.0.1</h2>
 			<ul>
 				<li>Initial release</li>
 				<li>Added CRUD Generator module for creating files according to a Controller-Service-Model pattern.</li>
